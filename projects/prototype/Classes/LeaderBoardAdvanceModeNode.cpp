2981655f01c63f028db3030e1a4ac037095ee46b
vandao <van.dao@kiss-concept.com>
2014-05-09 17:29:56 +0700

    add layout leader board on andvance mode

--- projects/prototype/Classes/LeaderBoardAdvanceModeNode.cpp
+++ projects/prototype/Classes/LeaderBoardAdvanceModeNode.cpp
@@ -80,16 +80,17 @@ bool LeaderBoardAdvanceModeNode::init()
 		m_iConnectServer = UserDefault::getInstance()->getIntegerForKey("NumberConnectServer", 0);
 		m_iConnectServer++;
 		UserDefault::getInstance()->setIntegerForKey("NumberConnectServer", m_iConnectServer);
-		UserService::getInstance()->getLeaderBoardLevel(m_sChapterId ,m_iLevel, m_iConnectServer);
+		UserService::getInstance()->getLeaderBoardAdvanceMode(m_sPackageId, m_iConnectServer);
 	}
 #endif
 
+	/*
 	UserService::getInstance()->addCallBackList(this);
 	m_iConnectServer = UserDefault::getInstance()->getIntegerForKey("NumberConnectServer", 0);
 	m_iConnectServer++;
 	UserDefault::getInstance()->setIntegerForKey("NumberConnectServer", m_iConnectServer);
 	UserService::getInstance()->getLeaderBoardAdvanceMode(m_sPackageId, m_iConnectServer);
-	
+	*/
 
 	return true;
 }
